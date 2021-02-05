629a9a9ffcac90600be1c25c2da6a2411f5d1f51
Artem3423 <art20032345@gmail.com>
2021-02-06 02:51:07 +0300

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println(12345);
+        System.out.println("Введите m и n:");
+        Scanner in = new Scanner(System.in);
+        double m_n = in.nextDouble();
+        double n_n = in.nextDouble();
+        double x;
+        x = ((m_n - 1) * Math.sqrt(m_n) - (n_n - 1) * Math.sqrt(n_n)) / (Math.sqrt(Math.pow(m_n, 3) * n_n) + (n_n * m_n) + Math.pow(m_n, 2) - m_n);
+        System.out.println("Ответ: " + x);
     }
 }
