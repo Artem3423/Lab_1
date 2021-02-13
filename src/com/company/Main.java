71bd33b29fe0c1c5b1d2bfe716468cc195cde374
Artem3423 <art20032345@gmail.com>
2021-02-13 11:19:25 +0300

    Lab_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,15 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Введите m и n:");
         Scanner in = new Scanner(System.in);
-        double m_n = in.nextDouble();
-        double n_n = in.nextDouble();
-        double x;
-        x = ((m_n - 1) * Math.sqrt(m_n) - (n_n - 1) * Math.sqrt(n_n)) / (Math.sqrt(Math.pow(m_n, 3) * n_n) + (n_n * m_n) + Math.pow(m_n, 2) - m_n);
-        System.out.println("Ответ: " + x);
+        int n = in.nextInt();
+        int x;
+        int[] nums = new int[n];
+        for(int i = 1; i < n; i++)
+        {
+            x = 2 * i - 1;
+            nums[i - 1] = x;
+            System.out.println("Ответ: " + nums[i-1]);
+        }
+
     }
 }
